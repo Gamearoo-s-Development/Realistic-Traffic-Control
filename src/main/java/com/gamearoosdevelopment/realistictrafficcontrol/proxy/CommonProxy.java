438f2cd81947d556d50a9deef7e8cc9e73e4d198
GamearooCodes <50187502+GamearooCodes@users.noreply.github.com>
2023-09-24 21:44:00 -0400

    Fixes

--- src/main/java/com/gamearoosdevelopment/realistictrafficcontrol/proxy/CommonProxy.java
+++ src/main/java/com/gamearoosdevelopment/realistictrafficcontrol/proxy/CommonProxy.java
@@ -301,7 +301,7 @@ public class CommonProxy {
 		
 
 		ModSounds.initSounds();
-		PacketHandler.registerMessages("realistictrafficcontrol");
+		PacketHandler.registerMessages("trafficcontrol");
 	}
 
 	public void init(FMLInitializationEvent e)
