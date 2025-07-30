cefc91c612371cb777a515afe40d812c8d6c5af8
Gamearoo Codes <gamearoo10@gmail.com>
2025-07-29 21:21:09 -0400

    s

--- 1.12.2/src/main/java/com/gamearoosdevelopment/realistictrafficcontrol/proxy/TrafficLightPeripheralProvider.java
+++ 1.12.2/src/main/java/com/gamearoosdevelopment/realistictrafficcontrol/proxy/TrafficLightPeripheralProvider.java
@@ -1,22 +1,31 @@
 package com.gamearoosdevelopment.realistictrafficcontrol.proxy;
 
+import com.gamearoosdevelopment.realistictrafficcontrol.ModRealisticTrafficControl;
 import com.gamearoosdevelopment.realistictrafficcontrol.CC.TrafficLightCardPeripheral;
 import com.gamearoosdevelopment.realistictrafficcontrol.tileentity.TrafficLightControlBoxTileEntity;
-import dan200.computercraft.api.peripheral.IPeripheral;
-import dan200.computercraft.api.peripheral.IPeripheralProvider;
+
+
+
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class TrafficLightPeripheralProvider implements IPeripheralProvider {
-
-    @Override
-    public IPeripheral getPeripheral(World world, BlockPos pos, EnumFacing side) {
-        TileEntity tile = world.getTileEntity(pos);
-        if (tile instanceof TrafficLightControlBoxTileEntity) {
-            return new TrafficLightCardPeripheral(world, pos, (TrafficLightControlBoxTileEntity) tile);
+public class TrafficLightPeripheralProvider {
+    public static void register() {
+        if (ModRealisticTrafficControl.CC_INSTALLED) {
+            dan200.computercraft.api.ComputerCraftAPI.registerPeripheralProvider(
+                new dan200.computercraft.api.peripheral.IPeripheralProvider() {
+                    @Override
+                    public dan200.computercraft.api.peripheral.IPeripheral getPeripheral(World world, BlockPos pos, EnumFacing side) {
+                        TileEntity tile = world.getTileEntity(pos);
+                        if (tile instanceof TrafficLightControlBoxTileEntity) {
+                            return new TrafficLightCardPeripheral(world, pos, (TrafficLightControlBoxTileEntity) tile);
+                        }
+                        return null;
+                    }
+                }
+            );
         }
-        return null;
     }
 }
