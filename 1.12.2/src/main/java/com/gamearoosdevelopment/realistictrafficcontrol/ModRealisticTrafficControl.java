8dff83a36cd95059b0956519699991d0293b82c8
Gamearoo Codes <gamearoo10@gmail.com>
2026-01-16 00:31:21 -0500

    version change

--- 1.12.2/src/main/java/com/gamearoosdevelopment/realistictrafficcontrol/ModRealisticTrafficControl.java
+++ 1.12.2/src/main/java/com/gamearoosdevelopment/realistictrafficcontrol/ModRealisticTrafficControl.java
@@ -34,7 +34,7 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod(modid = "realistictrafficcontrol", version = ModRealisticTrafficControl.VERSION, name = "Realistic Traffic Control", useMetadata = true,  guiFactory = "com.gamearoosdevelopment.realistictrafficcontrol.client.ModGuiFactory")
 public class ModRealisticTrafficControl {
 	public static final String MODID = "realistictrafficcontrol";
-	public static final String VERSION = "3.0.0";
+	public static final String VERSION = "3.1.0";
 	public static final String MODNAME = "Realistic Traffic Control";
 	public static boolean IR_INSTALLED = false;
 	public static boolean OC_INSTALLED = false;
